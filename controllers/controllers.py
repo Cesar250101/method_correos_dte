0b92f8c5ec3797babc1a5718c1e3f6d555d06215
Your Name <you@example.com>
2021-05-21 16:31:30 -0400

    first commit

---
+++ controllers/controllers.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# from odoo import http
+# from odoo.http import request
+# from odoo import models, fields, api
+
+# class MethodCorreosDte(http.Controller):
+    
+#     @http.route('/method_correos_dte', auth='public',website=True)
+#     def index(self, **kw):
+#         rut=request.params['rut']
+#         email_dte=self.env['correos_dte'].search([('rut','=',rut)])
+#         return email_dte
+
---
+++ models/models.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api
+import xmlrpc.client
+
+class method_correos_dte(models.Model):
+    _name = 'method_correos_dte.correos_dte'
+
+    name = fields.Char(string="Nombre del contribuyente")
+    rut = fields.Char(string="Rut del contribuyente")
+    nro_resolucion = fields.Char(string="Número Resolucion del SII")
+    fecha_resolucion = fields.Date(string='Fecha Resolucion del SII')
+    email_dte = fields.Char(string='Correo Electrónico de Intercambio')
+    url = fields.Char(string="URL contribuyente")
+    
+
+
+class ModuleName(models.Model):
+    _inherit = 'res.partner'
+
+    @api.one
+    def obtener_dte_email(self):
+        url = 'http://localhost:8069'
+        db = 'odoo12'
+        username = 'cesar@method.cl'
+        password = '2010'
+        
+        common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url))
+        models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url))
+        uid = common.authenticate(db, username, password, {})
+        dte_email=models.execute_kw(db, uid, password,
+            'method_correos_dte.correos_dte', 'search_read',
+            [[['rut', '=', self.document_number.replace('.','')]]],
+            [])
+        if dte_email:
+            for i in dte_email:
+                self.dte_email=i['email_dte']
+
